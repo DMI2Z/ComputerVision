9d4002c5654561dea32a68571a494b7dbfce0bcc
DMI2Z <58225322+DMI2Z@users.noreply.github.com>
2019-11-30 23:22:34 +0300

    Add files via upload

--- ComputerVision.py
+++ ComputerVision.py
@@ -10,8 +10,14 @@ import numpy as np
 parser = argparse.ArgumentParser()
 parser.add_argument('-d', "--debug", action="store_true", default=False)
 parser.add_argument('-s', "--show", type=str, choices=["remote", "local"], action="store", default=False)
-parser.add_argument('none')
-args = parser.parse_args()
+argv = parser.parse_args()
+
+
+if argv.debug:
+    DEBUG_COLOR = (255, 0, 0)
+
+if not argv.show:
+    import msvcrt
 
 
 # Константы
@@ -29,9 +35,15 @@ COLORS = {
     "yellow": [np.array((25, 250, 250), np.uint8), np.array((35, 255, 255), np.uint8)],
     "red": [np.array((0, 250, 250), np.uint8), np.array((10, 255, 255), np.uint8)]
 }
+CC_DATA = {}
 
-if args.debug:
-    DEBUG_COLOR = (255, 0, 0)
+
+# Фунция для работы с окнами в cv2.namedWindow
+# Исключительно для отладки
+def mouseCallback(event, x, y, flag, param) -> None:
+    # Ставит камеру на паузу
+    if event == cv2.EVENT_LBUTTONDBLCLK:
+        CC_DATA[param][0] = not CC_DATA[param][0]
 
 
 # Функция поиска контуров в кадре
@@ -79,7 +91,7 @@ def getLetterFromFrame(frame: np.ndarray) -> str:
         for letter in TEMPLATES:
             match = cv2.matchTemplate(rect, TEMPLATES[letter], cv2.TM_CCOEFF_NORMED)
             if match[0][0] >= TEMPLATE_MATCH_VALUE:
-                if args.debug:
+                if argv.debug:
                     cv2.rectangle(frame, (x-3, y-3), (x+w+3, y+h+3), DEBUG_COLOR, 2)
                 return letter
 
@@ -99,7 +111,7 @@ def getColorFromFrame(frame: np.ndarray) -> str:
 
         if moments['m00'] > COLOR_AREA:
 
-            if args.debug:
+            if argv.debug:
                 x = int(moments['m10'] / moments['m00'])
                 y = int(moments['m01'] / moments['m00'])
                 cv2.circle(frame, (x, y), 10, DEBUG_COLOR, -1)
@@ -109,18 +121,78 @@ def getColorFromFrame(frame: np.ndarray) -> str:
     return ""
 
 
+# Генератор захвата камеры
+# Принимает обьект cv2.VideoCapture и тэг функции
+# Тэг нужен что бы понять какая камера видит цвет или букву
+# Генерирует словарь (dict) вида:
+# {тэг: [статус, буква, цвет] }, пример: {"camera_left: [True, "H", "red"]}
+# статус показывает работает ли камера или нет
+def captureCamera(camera: cv2.VideoCapture, tag: str = None):
+    CC_DATA[tag] = [True, "", ""]
+    yield None
+
+    while camera.isOpened():
+        flag, frame = camera.read()
+
+        if flag and CC_DATA[tag][0]:
+
+            CC_DATA[tag] = [
+                True,
+                getLetterFromFrame(frame),
+                getColorFromFrame(frame)
+            ]
+
+            if argv.show:
+                cv2.imshow(tag, frame)
+
+        yield None
+
+
+# todo
+# Главная функция
+# аргуметы аргс и кв для красоты)
+# возвращает целое число, поже для обработки ошибок
+# когда поставим код на робота нужно будет переписать
+def main(*args, **kwargs) -> int:
+    cap = cv2.VideoCapture(0)
+
+    queue = [
+        captureCamera(cap, "Camera-first"),
+        captureCamera(cap, "Camera-second")
+    ]
+
+    # Запускаем камеры
+    for camera in queue:
+        camera.send(None)
+
+    # Привязываем фунцию обработки мыши
+    if argv.show:
+        for tag in CC_DATA:
+            cv2.namedWindow(tag)
+            cv2.setMouseCallback(tag, mouseCallback, param=tag)
+
+    while True:
+        for camera in queue:
+            camera.send(None)
+
+        if argv.show:
+            key = cv2.waitKey(1) & 0xFF
+            if key == ord('q'):
+                cv2.destroyAllWindows()
+                break
+
+        else:
+            if msvcrt.kbhit():
+                key = msvcrt.getch()
+                if key == b'1':
+                    break
+
+    cap.release()
+    return 0
+
+
 if __name__ == '__main__':
-    color_img = cv2.imread("images\\colors\\red.png")
-    letter_img = cv2.imread("images\\letters\\__Hus.png")
-
-    print(getLetterFromFrame(letter_img))
-    print(getColorFromFrame(color_img))
-
-    if args.show == 'local':
-        cv2.imshow("Color", color_img)
-        cv2.imshow("Letter", letter_img)
-        cv2.waitKey(0)
-        cv2.destroyAllWindows()
+    main()
 
 
 
--- TemplateCreator.py
+++ TemplateCreator.py
@@ -4,15 +4,45 @@
     Все функции после отладки переносятся в другие .py фалы
 
 """
+import cv2
+import os
+import msvcrt
 
-import argparse
-
-parser = argparse.ArgumentParser()
-parser.add_argument('-d', '--dis', action="store_true")
-args = parser.parse_args()
-
-print(args)
+CAMERAS_DATA = {}
+CAMERAS_QUEUE = []
 
 
+def captureCamera(tag: str = None):
+    CAMERAS_DATA.update({tag: [True, '', '']})
+
+    while tag in CAMERAS_DATA:
+        if CAMERAS_DATA[tag][0]:
+            yield print(tag)
+        else:
+            yield None
 
 
+if __name__ == '__main__':
+    CAMERAS_QUEUE = [
+        captureCamera("First"),
+        captureCamera("Second"),
+        captureCamera("Third")
+    ]
+
+    key = None
+    while key != b'27':
+        os.system("cls")
+        for camera in CAMERAS_QUEUE:
+            camera.send(None)
+
+        if msvcrt.kbhit():
+            key = msvcrt.getch()
+
+            if key == b'1':
+                CAMERAS_DATA["First"][0] = not CAMERAS_DATA["First"][0]
+
+            elif key == b'2':
+                CAMERAS_DATA["Second"][0] = not CAMERAS_DATA["Second"][0]
+
+            elif key == b'3':
+                CAMERAS_DATA["Third"][0] = not CAMERAS_DATA["Third"][0]
